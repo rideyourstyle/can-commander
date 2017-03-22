552641f72a7f9c2615c569fe90914b9264009948
peter.toenz <peter.toenz@noser.com>
2017-03-22 14:43:29 +0100

    added can virtual demo

--- start.py
+++ start.py
@@ -1,6 +1,13 @@
 import can
 
-if __name__ == "__main__":
-    print("Hello Python")
-    
-    
+bus1 = can.interface.Bus('test', bustype='virtual')
+bus2 = can.interface.Bus('test', bustype='virtual')
+
+msg1 = can.Message(arbitration_id=0xabcde, data=[1,2,3])
+bus1.send(msg1)
+msg2 = bus2.recv()
+
+print(msg1)
+print(msg2)
+
+
